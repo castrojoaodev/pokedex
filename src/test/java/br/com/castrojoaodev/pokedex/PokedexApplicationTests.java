867c6928375a2da17d725b0b33ce46d9b0a54967
castrojoaodev <castrojoao.dev@gmail.com>
2023-02-15 18:42:35 -0300

    Criação da API Pokédex

---
+++ src/test/java/br/com/castrojoaodev/pokedex/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.castrojoaodev.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
